38df6dcdfcceb9839434fdb15034868275df2edb
Sarah Chastain <schastain@gwu.edu>
2021-05-05 18:10:28 +0200

    mid edit of get_obstimes to fix gaps and cleanup

--- get_obstimes.py
+++ get_obstimes.py
@@ -4,12 +4,18 @@ from casatools import msmetadata, ms
 import sys
 from astropy.coordinates import SkyCoord
 import argparse
+
 parser=argparse.ArgumentParser(
     description='''Crude script that pulls info from supplied ms or multiple ms using unix wildcards. Always check output. Requires casa6 and astropy.''',
-    epilog="""Reads in input from sys.argv[1:]""")
-observations = sys.argv[1:]
+    epilog="""Reads in input from a txt file containing ms folder names via the --obs flag""")
+parser.add_argument("--obs", help="txt file containing a list of ms")
+
+# just gets a timestamp for a unique filename
+with open(parser.parse_args().obs) as msfile:
+
 starttime = datetime.datetime.now().strftime('%y%m%d%Hh%Mm%Ss')
 for targetobs in observations:
+# This somwhat unexpectedly elaborate function to get integration time is necessary because MS are very particular and frustrating to work with
     def get_integrationtime(s):
         print(s)
         msobj = ms()
@@ -20,6 +26,7 @@ for targetobs in observations:
             return get_integrationtime(s+1)
 
     try:
+ # s here doesn't really matter much it just sets an index that will be incremented if the function fails, so start low
         integration_time = get_integrationtime(1)
         start_epoch = datetime.datetime(1858, 11, 17, 00, 00, 00, 00)
         msmd = msmetadata()
@@ -30,6 +37,7 @@ for targetobs in observations:
             for s in msmd.scansforfield(f):
                 tmptimelist = []
                 for t in msmd.timesforscan(s):
+# convert timestamp to datetime format using the epoch time above
                     tmptimelist.append((start_epoch + datetime.timedelta(seconds=t)))
                 pointdict = msmd.phasecenter(f)
                 pointdir = SkyCoord([pointdict['m0']['value']],[pointdict['m1']['value']], frame=pointdict['refer'].replace('J2000','fk5'), unit=pointdict['m0']['unit'])
