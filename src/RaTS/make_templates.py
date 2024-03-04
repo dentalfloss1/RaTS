fa232489b9bbccf055556b97acdcf1de5c67a53f
Sarah Chastain <sarahichastain@gmail.com>
2024-03-04 13:33:18 -0700

    fix config defaults

--- src/RaTS/make_templates.py
+++ src/RaTS/make_templates.py
@@ -8,9 +8,9 @@ fl_max = 5
 ; Fractional error in the simulated flux ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 flux_err = 1e-1  
 ; Minimum simulated duration, in same units as the duration in the observations file  
-dmin = 5e-7 
+dmin = 5e-2 
 ; Maximum simulated duration, in same units as the duration in the observations file     
-dmax = 5e-2
+dmax = 5e3
 ; detection threshold, to be multiplied by the noise in the images
 det_threshold = 5 
 ; integer, extra detection threshold
@@ -31,10 +31,10 @@ detections = 5
 ; Number of observations to be simulated 
 nobs =  46
 ; Average sensitivity of simulated observations in Jy 
-obssens = 21.7e6
-; std dev of simulated sens in Jy 4.6e-6 4.6e-6 
-obssig = 4.6e6
+obssens = 21.7e-6
+; std dev of simulated sens in Jy 
+obssig = 4.6e-6
 ; interval between simulated observations in days 
 obsinterval = 7
 ; duration of observations in days 
-obsdurations =  0.009 """
+obsdurations =  0.009"""
