21bb690205120b239b62dc80fcb875df4747b8d0
Sarah Chastain <sarahchastain1@unm.edu>
2024-07-09 12:02:25 -0600

    Refactor (#7)
    * finished initial refactor
    
    * fix config defaults
    
    * restructure
    
    * fix import
    
    ---------
    
    Co-authored-by: Sarah Chastain <sarahichastain@gmail.com>

---
+++ RaTS/make_templates.py
@@ -0,0 +1,40 @@
+configfilestring="""[INITIAL PARAMETERS]
+; integer number of sources to be simulated
+n_sources = 2e5
+; Minimum simulated flux, in same units as the flux in the observations file ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+fl_min = 5e-5
+; Maximum simulated flux ,in same units as the flux in the observations file  
+fl_max = 5  
+; Fractional error in the simulated flux ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+flux_err = 1e-1  
+; Minimum simulated duration, in same units as the duration in the observations file  
+dmin = 5e-2 
+; Maximum simulated duration, in same units as the duration in the observations file     
+dmax = 5e3
+; detection threshold, to be multiplied by the noise in the images
+det_threshold = 5 
+; integer, extra detection threshold
+extra_threshold = 0
+; Name to be used for the output files
+file = allint  
+; Must be present as a python file 
+lightcurvetype = tophat
+; only used for choppedgaussian
+gaussiancutoff = 0.1
+; confidence levels for limits
+confidence = 95
+; Upper limit on detections. 3 is 95% confidence
+detections = 5
+
+; The following is only used if no observations are provided in an observations file
+[SIM]
+; Number of observations to be simulated 
+nobs =  46
+; Average sensitivity of simulated observations in Jy 
+obssens = 21.7e-6
+; std dev of simulated sens in Jy 
+obssig = 4.6e-6
+; interval between simulated observations in days 
+obsinterval = 7
+; duration of observations in days 
+obsdurations =  0.009"""
