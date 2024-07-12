b59401c858b7449f3ab8461995cbf3573a6e91ba
Sarah Chastain <sarahchastain1@unm.edu>
2024-07-12 14:07:39 -0600

    implement c version of sbpl

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ import glob
 
 from setuptools import setup, Extension, Distribution, find_packages
 
+module1 = Extension('RaTS/sbpl',['RaTS/sbplmodule.c'],
+                     extra_link_args=["-lm","-lgsl","-lgslcblas"])
 
 setup(name="RaTS",
       version="1.0",
@@ -12,6 +14,7 @@ setup(name="RaTS",
       scripts = glob.glob('scripts/*.py'),
       python_requires ='>3.6',
       install_requires = ["astropy","bitarray","colorama","cycler","fonttools","kiwisolver","matplotlib","numpy","packaging","Pillow","pyerfa","pyparsing","python-dateutil","PyYAML","scipy","six","tqdm"],
-      test_suite="tests")
+      test_suite="tests",
+      ext_modules=[module1])
 
 
